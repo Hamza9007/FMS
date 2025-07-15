ad9f83a3f67bde9405121d87691dfeb8898d21f3
Hamza9007 <armaanw22@gmail.com>
2025-07-15 11:33:59 +0500

    1.0.0.4

--- adminHeader.php
+++ adminHeader.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel</title>
+    <title><?= $_SESSION['username'];?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/admin.css">
 </head>
