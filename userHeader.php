6385ee6caad44a5c0f68ba9278246115ab0591d7
Hamza9007 <armaanw22@gmail.com>
2025-07-15 11:35:40 +0500

    1.0.0.4

--- userHeader.php
+++ userHeader.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title class="family">User Panel</title>
+    <title class="family"><?= $_SESSION['username'];?></title>
     <link rel="stylesheet" href="css/user.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
