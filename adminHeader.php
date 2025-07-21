10aadd23c0e443eba37326fdaf0a67302eab8b89
Hamza9007 <armaanw22@gmail.com>
2025-07-21 14:16:59 +0500

    1.0.0.8

--- adminHeader.php
+++ adminHeader.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="30">
+    <meta http-equiv="refresh" content="60">
     <title><?= $_SESSION['username'];?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
